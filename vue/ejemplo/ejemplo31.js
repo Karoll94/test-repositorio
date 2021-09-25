33130bd744e873c3790fe0a5b3f22a568cb53595
Karoll94 <karollmartin64@gmail.com>
2021-09-25 16:19:35 -0500

    ultimo vue

---
+++ vue/ejemplo/ejemplo31.js
@@ -0,0 +1,23 @@
+const app = new Vue ({
+    el:'#app',
+    data:{
+        titulo:"Hola muchachos",
+        equipos : [
+            {
+                nombre: 'Colombia',
+                titulos: 0
+            },
+            
+            {
+                nombre: 'Alemania',
+                titulos: 4
+            },
+            
+            {
+                nombre: 'Brasil',
+                titulos: 5
+            }
+        ]
+        
+    }
+})
