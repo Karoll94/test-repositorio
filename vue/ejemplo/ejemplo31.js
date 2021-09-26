a79048ac3e4055b371efe351ae6a1e9050497bc6
Karoll94 <karollmartin64@gmail.com>
2021-09-26 18:58:22 -0500

    clase26/09/2021

--- vue/ejemplo/ejemplo31.js
+++ vue/ejemplo/ejemplo31.js
@@ -3,21 +3,30 @@ const app = new Vue ({
     data:{
         titulo:"Hola muchachos",
         equipos : [
-            {
-                nombre: 'Colombia',
-                titulos: 0
-            },
-            
-            {
-                nombre: 'Alemania',
-                titulos: 4
-            },
-            
-            {
-                nombre: 'Brasil',
-                titulos: 5
+            {nombre: 'Colombia',titulos: 0},
+            {nombre: 'Alemania',titulos: 4},
+            {nombre: 'Brasil',titulos: 5} 
+        ],
+        nuevoEquipo:'' 
+    },
+    methods: {
+        agregarEquipo(){
+            this.equipos.push({
+                nombre:this.nuevoEquipo,titulo:0
+            });
+            this.nuevoEquipo ='';
+        }
+    },
+    computed:{
+        sumarCopas(){
+            this.total=0;
+            for(equipo of this.equipos){
+                this.total = this.total + equipo.titulos;
             }
-        ]
-        
+            return this.total;
+        }
     }
-})
+});
+Vue.component('saludo',{
+    template:'<h1>Hola muchachos</h1>'
+});
